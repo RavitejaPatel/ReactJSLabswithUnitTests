b97fa15163f89f5a3a7fb22c5157089bff385a7a
Raviteja <Raviteja.idams134@gmail.com>
2023-09-09 10:36:32 -0700

    added dev note and ex's on rest operator

--- ES6_7Concepts/es6concepts.js
+++ ES6_7Concepts/es6concepts.js
@@ -12,3 +12,26 @@ detailsInfo = {
 }
 const { worklocation, fname } = detailsInfo
 console.log(worklocation)
+
+
+//=================================
+//Rest Operator javascript
+
+/**
+ * DEV NOTE :
+ * here arr keyword is fixed and if you try keppig it without name, you get error 
+ * Rest param(...arr) always has to define at last ad can mentio other params at beginig , ex2
+ * **/
+const restOperatorFun = (...arr) => {
+    console.log(arr)
+}
+restOperatorFun(10, 100, "hello")
+
+
+const restOperatorFunwithMultiParams = (name, message, ...arr) => {
+    console.log(arr)
+    console.log(name, message)
+}
+restOperatorFunwithMultiParams("Raviteja", "hello dev", 10, 100)
+
+
